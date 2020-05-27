c71ea221290d60fa9234b7b5ea0be6a196b4fdb2
7-Spades <edwin5106@gmail.com>
2020-05-26 22:36:07 -0500

    added axios search and input handlers

--- book-react/src/components/searchBar.js
+++ book-react/src/components/searchBar.js
@@ -1,20 +1,42 @@
 import React from 'react'
+import axios from 'axios'
 
-function searchbar(){
+class Searchbar extends React.Component{
+  state = {
+    searcher: ''
+  }
+
+  inputHandler = (event) => {
+
+    this.setState({
+      searcher: event.target.value
+    })
+  }
+
+  submitHandler= (event) => {
+    event.preventDefault();
+    let apikey= 'AIzaSyCJvBEYRG0k6yjCp3U7dJNpFsFDmEbaCSc'
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${this.state.searcher}key=${apikey}`)
+    .then(res => {console.log(res.data)})
+    .catch(err => {console.log(err)})
+  }
+
+  render(){
     return(
         <div className='container'>
         <div className='row'>
         <div className='col'>
         <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="What book are you looking for?" />
+        <input onChange= {this.inputHandler} value={this.state.searcher} type="text" class="form-control" placeholder="What book are you looking for?" />
         <div class="input-group-append">
-          <button class="btn btn-outline-secondary" type="button" id="submit-button">Search</button>
+          <button class="btn btn-outline-secondary" onClick={this.submitHandler} type="button" id="submit-button">Search</button>
         </div>
       </div>
     </div>
     </div>
     </div>
     )
+  }
 }
 
-export default searchbar
+export default Searchbar
